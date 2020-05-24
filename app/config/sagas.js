150744561fe99ff5f2d8cd8eea6b1c7d78140a19
kn-neeraj <kn.neeraj.89@gmail.com>
2020-05-24 20:19:14 +0530

    Fetching data from API and updating the UI

--- app/config/sagas.js
+++ app/config/sagas.js
@@ -1,13 +1,35 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, select, call, put } from 'redux-saga/effects';
 
 // 1. when the base experience is changed
 // 2. initially get conversion information
 
-import { CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION } from '../actions/currencies'
+import { CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION, CONVERSION_RESULT, CONVERSION_ERROR } from '../actions/currencies'
 
+const URL_BASE = "https://api.frankfurter.app/"
+const URL_BASE_LATEST = "latest"
+
+export const getLatestRate = currency => {
+    let url = URL_BASE + URL_BASE_LATEST + "?" + "base=" + String(currency);
+    return fetch(url);
+}
 function* fetchLatestConversionRates(action) {
-    console.log("TODO: Update the things", action);
-    yield;
+    try {
+        let currency = action.currency;
+        if (currency === undefined) {
+            currency = yield select(state => state.currencies.baseCurrency);
+        }
+        const response = yield call(getLatestRate, currency);
+        const result = yield response.json()
+        if (result.error) {
+            yield put({ type: CONVERSION_ERROR, error: result.error });
+        } else {
+            yield put({ type: CONVERSION_RESULT, result })
+        }
+    } catch (e) {
+        console.log('Saga error', e);
+        yield put({ type: CONVERSION_ERROR, error: e.message });
+    }
+
 }
 
 //generator
